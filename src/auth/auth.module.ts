b78172f0de6942ed318cb12b02b5ecbfc7de6423
Minh Truong <minhtruongdevvn@gmail.com>
2023-03-08 08:31:58 +0700

    ✨ feat(auth): re-enforce auth
    add jwt authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AuthStrategy } from './strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AuthStrategy],
   imports: [
     /*PrismaModule*/
+    JwtModule.register({}),
   ],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,10 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt/dist';
 import { Prisma } from '@prisma/client';
 import * as argon from 'argon2';
 import constant from 'src/constant';
@@ -7,7 +13,11 @@ import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async login(dto: AuthDto) {
     const user = await this.prisma.user.findUnique({
@@ -16,13 +26,12 @@ export class AuthService {
       },
     });
 
-    if (!user) throw new ForbiddenException('Credentials incorrect');
+    if (!user) throw new UnauthorizedException('Credentials incorrect');
 
     if (!(await argon.verify(user.hash, dto.password)))
-      throw new ForbiddenException('Credentials incorrect');
+      throw new UnauthorizedException('Credentials incorrect');
 
-    delete user.hash;
-    return user;
+    return await this.signToken(user.id, user.email);
   }
 
   async signup(dto: AuthDto) {
@@ -34,15 +43,30 @@ export class AuthService {
           hash,
         },
       });
-      delete user.hash;
 
-      return user;
+      return await this.signToken(user.id, user.email);
     } catch (err) {
       if (err instanceof Prisma.PrismaClientKnownRequestError) {
         if (err.code == constant.error.dbDupplicateErrorCode)
-          throw new ForbiddenException('Credentials taken');
+          throw new BadRequestException('Credentials taken');
       }
       throw err;
     }
+  }
+
+  async signToken(userId: number, email: string) {
+    const payload = {
+      sub: userId,
+      email: email,
+    };
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: this.config.get('JWT_SIGN_SECRET'),
+    });
+
+    return {
+      access_token: token,
+    };
   }
 }
