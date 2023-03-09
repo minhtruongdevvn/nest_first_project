4791398b20a96c5563f5bd556c408aa772956faf
Minh Truong <minhtruongdevvn@gmail.com>
2023-03-09 09:35:27 +0700

    ✨ feat(user): crud user
    add get and edit user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,31 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
+import { UserService } from './user.service';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
   @Get('me')
+  @HttpCode(HttpStatus.OK)
   getMe(/* @Req() req: Request */ @GetUser() user: User) {
     // req.user
     return user;
+  }
+
+  @Put('me')
+  @HttpCode(HttpStatus.OK)
+  editMe(@Body() dto: { email: string }, @GetUser() user: User) {
+    return this.userService.editUser(user.id, dto);
   }
 }
