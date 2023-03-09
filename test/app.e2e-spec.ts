4233520ffcfc45f734284387581b6300c7b1ef21
Minh Truong <minhtruongdevvn@gmail.com>
2023-03-09 11:03:59 +0700

    🧪 test(bookmark): bookmark testing
    add e2e test cases for bookmark api

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,6 +3,7 @@ import { Test } from '@nestjs/testing';
 import * as pactum from 'pactum';
 import { AppModule } from 'src/app.module';
 import { AuthDto } from 'src/auth/dto';
+import { CreateBookmarkDto, EditBookmarkDto } from 'src/bookmark/dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { EditUserDto } from 'src/user/dto/edit-user.dto';
 
@@ -131,10 +132,170 @@ describe('App e2e', () => {
     });
   });
   describe('Bookmarks', () => {
-    describe('Create bookmark', () => {});
-    describe('Get bookmarks', () => {});
-    describe('Get bookmark by id', () => {});
-    describe('Edit bookmark', () => {});
-    describe('Delete bookmark', () => {});
+    describe('Get empty bookmark', () => {
+      it('should return empty', async () => {
+        await pactum
+          .spec()
+          .get('/bookmarks/me')
+          .withHeaders('Authorization', 'Bearer $S{userAt}')
+          .expectBody([]);
+      });
+    });
+
+    describe('Create bookmark', () => {
+      const createDto: CreateBookmarkDto = {
+        title: 'test bookmark title',
+        description: 'test bookmark description',
+        link: 'test bookmark link',
+      };
+
+      it('should throw if unauthorized', async () => {
+        await pactum
+          .spec()
+          .post('/bookmarks')
+          .withBody(createDto)
+          .expectStatus(HttpStatus.UNAUTHORIZED);
+      });
+
+      it('should throw if validation failed create bookmarks', async () => {
+        await pactum
+          .spec()
+          .post('/bookmarks')
+          .withHeaders('Authorization', 'Bearer $S{userAt}')
+          .withBody({ ...createDto, link: '' })
+          .expectStatus(HttpStatus.BAD_REQUEST);
+      });
+
+      it('should create bookmarks', async () => {
+        await pactum
+          .spec()
+          .post('/bookmarks')
+          .withHeaders('Authorization', 'Bearer $S{userAt}')
+          .withBody(createDto)
+          .expectStatus(HttpStatus.CREATED)
+          .stores('bookmarkId', 'id')
+          .stores('bookmarkTitle', 'title');
+      });
+    });
+
+    describe('Get bookmarks', () => {
+      it('should throw if unauthorized', async () => {
+        await pactum
+          .spec()
+          .get('/bookmarks/me')
+          .expectStatus(HttpStatus.UNAUTHORIZED);
+      });
+
+      it('should return user bookmarks', async () => {
+        await pactum
+          .spec()
+          .get('/bookmarks/me')
+          .withHeaders('Authorization', 'Bearer $S{userAt}')
+          .expectStatus(HttpStatus.OK)
+          .expectJsonLength(1);
+      });
+    });
+
+    describe('Get bookmark by id', () => {
+      it('should return empty if bookmark not found', async () => {
+        await pactum
+          .spec()
+          .get('/bookmarks/99999')
+          .expectStatus(HttpStatus.OK)
+          .expectBody('');
+      });
+
+      it('should return bookmark of requested id', async () => {
+        await pactum
+          .spec()
+          //.get('/bookmarks/$S{bookmarkId}')
+          .get('/bookmarks/{id}')
+          .withPathParams('id', '$S{bookmarkId}')
+          .expectStatus(HttpStatus.OK)
+          .expectBodyContains('$S{bookmarkId}')
+          .expectBodyContains('$S{bookmarkTitle}');
+      });
+    });
+
+    beforeAll(async () => {
+      await pactum
+        .spec()
+        .post('/auth/signup')
+        .withBody({
+          email: 'test2@e.com',
+          password: '123',
+        })
+        .expectStatus(201)
+        .stores('user2At', 'access_token');
+    });
+
+    describe('Edit bookmark', () => {
+      const editDto: EditBookmarkDto = {
+        title: 'test edit bookmark title',
+        link: 'test edit bookmark link',
+      };
+
+      it('should thow if user is not owner', async () => {
+        await pactum
+          .spec()
+          .put('/bookmarks/$S{bookmarkId}')
+          .withHeaders('Authorization', 'Bearer $S{user2At}')
+          .withBody(editDto)
+          .expectStatus(HttpStatus.BAD_REQUEST);
+      });
+
+      it('should throw if bookmark not found', async () => {
+        await pactum
+          .spec()
+          .put('/bookmarks/99999')
+          .withHeaders('Authorization', 'Bearer $S{userAt}')
+          .withBody(editDto)
+          .expectStatus(HttpStatus.BAD_REQUEST);
+      });
+
+      it('should edit successfully', async () => {
+        await pactum
+          .spec()
+          .put('/bookmarks/$S{bookmarkId}')
+          .withHeaders('Authorization', 'Bearer $S{userAt}')
+          .withBody(editDto)
+          .expectStatus(HttpStatus.OK)
+          .expectBodyContains(editDto.title)
+          .expectBodyContains(editDto.link);
+      });
+    });
+
+    describe('Delete bookmark', () => {
+      it('should thow if user is not owner', async () => {
+        await pactum
+          .spec()
+          .delete('/bookmarks/$S{bookmarkId}')
+          .withHeaders('Authorization', 'Bearer $S{user2At}')
+          .expectStatus(HttpStatus.BAD_REQUEST);
+      });
+
+      it('should throw if bookmark not found', async () => {
+        await pactum
+          .spec()
+          .delete('/bookmarks/99999')
+          .withHeaders('Authorization', 'Bearer $S{userAt}')
+          .expectStatus(HttpStatus.BAD_REQUEST);
+      });
+
+      it('should delete successfully', async () => {
+        await pactum
+          .spec()
+          .delete('/bookmarks/$S{bookmarkId}')
+          .withHeaders('Authorization', 'Bearer $S{userAt}')
+          .expectStatus(HttpStatus.OK);
+
+        await pactum
+          .spec()
+          .get('/bookmarks/me')
+          .withHeaders('Authorization', 'Bearer $S{userAt}')
+          .expectStatus(HttpStatus.OK)
+          .expectBody([]);
+      });
+    });
   });
 });
